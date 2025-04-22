902b351c55d693ea687877ed51fb4f5d11769d83
mason-1009 <mmason55@protonmail.com>
2025-04-21 22:37:32 -0400

    rejectUnauthorized -> false

--- src/db.js
+++ src/db.js
@@ -3,7 +3,9 @@ const { Pool, Client } = require('pg');
 const connectionString = process.env.DATABASE_URL;
 const pgPool = new Pool({
   connectionString,
-  ssl: true
+  ssl: {
+    rejectUnauthorized: false,
+  }
 });
 
 module.exports = { pgPool };
