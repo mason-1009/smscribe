84c1774457b172eb67f1fdbed10970af576a38c6
mason-1009 <mmason55@protonmail.com>
2025-04-21 22:36:18 -0400

    Set SSL true

--- src/db.js
+++ src/db.js
@@ -1,6 +1,9 @@
 const { Pool, Client } = require('pg');
 
 const connectionString = process.env.DATABASE_URL;
-const pgPool = new Pool({ connectionString });
+const pgPool = new Pool({
+  connectionString,
+  ssl: true
+});
 
 module.exports = { pgPool };
